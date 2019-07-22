90073c33cc1bbda99d19b4cdae74e89f64d8ee07
sqy <1245@qq.com>
2019-07-22 09:19:52 +0800

    gen annotation

---
+++ gen_annotation.py
@@ -0,0 +1,20 @@
+"""
+generate annotation
+
+the result is a text file
+
+format:
+target_img_name,original_img_path,x1,y1,x2,y2,is_contain_target
+
+e.g.
+
+"""
+
+
+def select_target_imgs():
+    """
+    选择目标图片
+    :return:
+    """
+    pass
+
